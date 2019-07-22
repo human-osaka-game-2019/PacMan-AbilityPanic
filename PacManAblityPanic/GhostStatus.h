9669e7c2c8539e53b9efed39b2ec6bd6ec792388
上久保豪太 <kamikubo1123@gmail.com>
2019-07-22 13:52:37 +0900

    GhostStatus.hを追加

---
+++ PacManAblityPanic/GhostStatus.h
@@ -0,0 +1,18 @@
+﻿/**
+* @file GhostStatus.h
+* @brief ゴースト情報の宣言
+*/
+#ifndef GHOST_STATUS_H_
+#define GHOST_STATUS_H_
+
+/**
+* @brief ゴースト情報の継承元クラス
+*/
+class GHOST_COMMON_STATUS
+{
+public:
+	float	MovementSpeed;	//! ゴーストの移動速度
+	bool	LiveFlag;		//! ゴーストの生存フラグ
+};
+
+#endif
