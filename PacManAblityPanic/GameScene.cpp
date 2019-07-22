0e8a4d22e68fa4a018c9d0f3b6ef640c30190325
mono1140 <11potato30@gmail.com>
2019-07-22 16:14:44 +0900

    :sparkles: GameScene.cpp GameScene.h の基本コード(ゲームループ)の追加(#5)

--- PacManAblityPanic/GameScene.cpp
+++ PacManAblityPanic/GameScene.cpp
@@ -1,1 +1,50 @@
-﻿
+﻿#include<stdio.h>
+#include"Scene.h"
+
+extern int g_SceneStep;
+
+void InitGameScene();
+void UpdateGameScene();
+int FinisGameScene();
+
+void InitGameScene()
+{
+	printf("ゲームシーンの初期化 \n");
+
+	g_SceneStep = 1;
+}
+
+void UpdateGameScene()
+{
+	printf("ゲームシーンの本編 \n");
+
+	g_SceneStep = 2;
+}
+
+int FinisGameScene()
+{
+	printf("ゲームシーンの終了 \n");
+
+	// 次のシーンの遷移先IDを返す
+	return RESULT_SCENE_ID;
+}
+
+int GameSceneMain()
+{
+	switch (g_SceneStep)
+	{
+		// 初期化
+	case 0:
+		InitGameScene();
+		break;
+
+		// 本編
+	case 1:
+		UpdateGameScene();
+		break;
+		// 終了
+	case 2:
+		return FinisGameScene();
+	}
+	return GAME_SCENE_ID;
+}
--- PacManAblityPanic/GameScene.h
+++ PacManAblityPanic/GameScene.h
@@ -1,1 +1,6 @@
-﻿
+﻿#ifndef _GAMESCENE_H_ 
+#define _GAMESCENE_H_
+
+int GameSceneMain();
+
+#endif // !
