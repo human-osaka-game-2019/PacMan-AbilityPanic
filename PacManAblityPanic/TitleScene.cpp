e0f6df22d31d43646f6ec6077227b7d6b8fef6ec
mono1140 <11potato30@gmail.com>
2019-07-22 16:21:58 +0900

    :sparkles: TitleScene.cpp TitleScene.h の基本コード(ゲームループ)の追加(#2)

--- PacManAblityPanic/TitleScene.cpp
+++ PacManAblityPanic/TitleScene.cpp
@@ -1,1 +1,52 @@
-﻿
+﻿#include<stdio.h>
+#include"Scene.h"
+
+extern int g_SceneStep;
+
+void InitTitleScene();
+void UpdateTitleScene();
+int FinisTitleScene();
+
+void InitTitleScene()
+{
+	printf("タイトルシーンの初期化 \n");
+
+	g_SceneStep = 1;
+}
+
+void UpdateTitleScene()
+{
+	printf("タイトルシーンの本編 \n");
+
+	g_SceneStep = 2;
+}
+
+int FinisTitleScene()
+{
+	printf("タイトルシーンの終了 \n");
+
+	// 次のシーンの遷移先IDを返す
+	return GAME_SCENE_ID;
+}
+
+
+int TitleSceneMain()
+{
+	switch (g_SceneStep)
+	{
+		// 初期化
+	case 0:
+		InitTitleScene();
+		break;
+
+		// 本編
+	case 1:
+		UpdateTitleScene();
+		break;
+		// 終了
+	case 2:
+		return FinisTitleScene();
+		break;
+	}
+	return TITLE_SCENE_ID;
+}
--- PacManAblityPanic/TitleScene.h
+++ PacManAblityPanic/TitleScene.h
@@ -1,1 +1,6 @@
-﻿
+﻿#ifndef _TIRLESCENE_H_ 
+#define _TIRLESCENE_H_
+
+int TitleSceneMain();
+
+#endif // !
