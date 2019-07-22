b6728e3ff0354480e83dc2aa6ca4a63795c1badb
上久保豪太 <kamikubo1123@gmail.com>
2019-07-22 18:03:24 +0900

    :sparkles:ゴーストAIに関する記載を追加

--- PacManAblityPanic/GhostAi.cpp
+++ PacManAblityPanic/GhostAi.cpp
@@ -1,1 +1,14 @@
-﻿
+﻿#include"GhostStatus.h"
+#include"GhostAi.h"
+
+void RED_GHOST::RedGhostUpdate () {
+	switch ( RED_GHOST::mode)
+	{
+	case NormalMode:
+		break;
+	case ScatterMode:
+		break;
+	case IzikeMode:
+		break;
+	}
+}
--- PacManAblityPanic/GhostAi.h
+++ PacManAblityPanic/GhostAi.h
@@ -5,5 +5,11 @@
 #ifndef GHOST_AI_H_
 #define GHOST_AI_H_
 
+class RED_GHOST : public GHOST_BASE
+{
+public:
+	void RedGhostUpdate ();
+};
+
 #endif
 
