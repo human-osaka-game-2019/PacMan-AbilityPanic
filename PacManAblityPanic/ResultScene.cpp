f2f4f3b6a5191c7e8f7c46f5ba750af9bf266851
mono1140 <11potato30@gmail.com>
2019-07-22 16:20:49 +0900

    :sparkles: ResultScene.cpp ResultScene.h の基本コード(ゲームループ)の追加(#7)

--- PacManAblityPanic/ResultScene.cpp
+++ PacManAblityPanic/ResultScene.cpp
@@ -1,1 +1,50 @@
-﻿
+﻿#include<stdio.h>
+#include"Scene.h"
+
+extern int g_SceneStep;
+
+void InitResultScene();
+void UpdateResultScene();
+int FinisResultScene();
+
+void InitResultScene()
+{
+	printf("リザルトシーンの初期化 \n");
+
+	g_SceneStep = 1;
+}
+
+void UpdateResultScene()
+{
+	printf("リザルトシーンの本編 \n");
+
+	g_SceneStep = 2;
+}
+
+int FinisResultScene()
+{
+	printf("リザルトシーンの終了 \n");
+
+	// 次のシーンの遷移先IDを返す
+	return TITLE_SCENE_ID;
+}
+
+int ResultSceneMain()
+{
+	switch (g_SceneStep)
+	{
+		// 初期化
+	case 0:
+		InitResultScene();
+		break;
+
+		// 本編
+	case 1:
+		UpdateResultScene();
+		break;
+		// 終了
+	case 2:
+		return FinisResultScene();
+	}
+	return RESULT_SCENE_ID;
+}
