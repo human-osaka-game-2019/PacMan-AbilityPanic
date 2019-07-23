883152d93fe7ee3da2f979917c694dc0d231c798
mono1140 <11potato30@gmail.com>
2019-07-23 11:31:11 +0900

    :construction: Scene.cpp Scene.h に移行したMain.cpp Main.h の情報を修正し、テクスチャの描画設定を作成中

--- PacManAblityPanic/Scene.cpp
+++ PacManAblityPanic/Scene.cpp
@@ -1,1 +1,76 @@
-﻿
+﻿#include <stdio.h>
+#include <windows.h>
+#include <d3d9.h>
+
+#include "Main.h"
+#include "Scene.h"
+#include "TitleScene.h"
+#include "GameScene.h"
+#include "ResultScene.h"
+
+int g_SceneStep = 0;
+
+void main()
+{
+	int current_scene_id = TITLE_SCENE_ID;
+
+	while (true)
+	{
+		int result_id = current_scene_id;
+
+		switch (current_scene_id)
+		{
+		case TITLE_SCENE_ID:
+			result_id = TitleSceneMain();
+			break;
+		case GAME_SCENE_ID:
+			result_id = GameSceneMain();
+			break;
+		case RESULT_SCENE_ID:
+			result_id = ResultSceneMain();
+			break;
+		}
+
+
+
+		if (current_scene_id != result_id)
+		{
+			current_scene_id = result_id;
+			g_SceneStep = SceneStep::InitStep;
+		}
+	}
+}
+
+void DrawScene()
+{
+	LPDIRECT3DDEVICE9 pDinput;
+	if (DrawStart() == false)
+	{
+		return;
+	}
+	if (g_CurrentSceneStep == SceneStep::MainStep)
+	{
+		switch (g_CurrentSceneId)
+		{
+		case SceneId::TitleScene:
+			DrawTitleScene();
+			break;
+		case 0:
+			DrawGameScene();
+			break;
+		case 1:
+			DrawGameClearScene();
+			break;
+		case 2:
+			DrawGameOverScene();
+			break;
+		case 3:
+			DrawHelpScene();
+			break;
+		}
+	}
+
+
+	pDinput->EndScene();
+	pDinput->Present(NULL, NULL, NULL, NULL);
+}
--- PacManAblityPanic/Scene.h
+++ PacManAblityPanic/Scene.h
@@ -1,1 +1,15 @@
-﻿
+﻿#ifndef SCENE_H_
+#define SCENE_H_
+
+#define TITLE_SCENE_ID (0)
+#define GAME_SCENE_ID (1)
+#define RESULT_SCENE_ID (2)
+
+enum SceneStep
+{
+	InitStep,			// 初期化
+	MainStep,			// メイン
+	EndStep,			// 終了
+};
+
+#endif // !
