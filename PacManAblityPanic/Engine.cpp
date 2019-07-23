b51ea07c52162b3aca4396aa13acae76bb00e54d
mono1140 <11potato30@gmail.com>
2019-07-24 00:26:24 +0900

    :sparkles: windowの生成、デバイス生成をするた為の関数を入れてある Engine.cpp を作成

---
+++ PacManAblityPanic/Engine.cpp
@@ -0,0 +1,18 @@
+﻿#include "class.h"
+#include "window.h"
+#include "Device.h"
+
+bool InitEngine(int width, int height, HINSTANCE hInst)
+{
+	//	window生成
+	HWND wind_handle = Make_WndFull(hInst,width,height);
+
+	BuildDxDevice(wind_handle);
+
+	return false;
+}
+
+void EndEngine()
+{
+
+}
