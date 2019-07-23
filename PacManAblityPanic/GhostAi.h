b13ee747264cedfd35477c78dacb04d902a2c046
上久保豪太 <kamikubo1123@gmail.com>
2019-07-23 11:21:14 +0900

    :art: #10 RED_GHOSTクラスにdoxygen用コメントを追加

--- PacManAblityPanic/GhostAi.h
+++ PacManAblityPanic/GhostAi.h
@@ -4,10 +4,15 @@
 */
 #ifndef GHOST_AI_H_
 #define GHOST_AI_H_
-
+/**
+* @brief ブリンキー（赤いゴースト）のAI作成用クラス
+*/
 class RED_GHOST : public GHOST_BASE
 {
 public:
+	/**
+	* @brief 現在のモードによって対応する関数を実行する関数
+	*/
 	void RedGhostUpdate ();
 };
 
