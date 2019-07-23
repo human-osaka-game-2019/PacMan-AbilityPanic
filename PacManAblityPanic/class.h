43133ab9b6f69d31a3dd2a77fc83eea00a114b11
mono1140 <11potato30@gmail.com>
2019-07-24 00:22:51 +0900

    :sparkles: 構造体、クラスを入れる class.h を作成

---
+++ PacManAblityPanic/class.h
@@ -0,0 +1,50 @@
+﻿#ifndef CLASS_H
+#define CLASS_H
+
+#include<stdio.h>
+#include<windows.h>
+#include<d3dx9.h>
+#include<tchar.h>
+#include<dinput.h>
+
+typedef struct
+{
+	float	// 頂点座標
+		x,  // 幅
+		y,  // 高さ
+		z;  // 奥行
+
+	float   // 除算数
+		rhw;
+
+	DWORD
+		dwColor; // 頂点の色
+
+	float   // テクスチャ座標
+		tu,  // 幅
+		tv;	// 高さ
+} CUSTOMVERTEX, * P_CUSTOMVERTEX;
+
+class Pointa
+{
+public:
+	LPDIRECT3DTEXTURE9 pTexture;
+	LPDIRECT3D9 pDirect3d;
+	LPDIRECT3DDEVICE9 pDevice;
+	LPDIRECTINPUT8 pDinput;
+	LPDIRECTINPUTDEVICE8 pkey;
+
+};
+
+// テクスチャ //
+const int D3DFVF_CUSTOMVERTEX(D3DFVF_XYZRHW | D3DFVF_DIFFUSE | D3DFVF_TEX1);
+// テクスチャのFVF情報
+
+// キーボード //
+static const int MAX_KEY_NUMBER = 256;
+// 入力キーを入れる値
+BYTE KeyState[MAX_KEY_NUMBER];
+// 256の配列を持つ
+const int MASK_NUM = 0x80;
+
+#endif // !1
