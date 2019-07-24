3f06abb9504d31d212b548c4865b0e0f2ef17407
mono1140 <11potato30@gmail.com>
2019-07-24 14:39:49 +0900

    :sparkles: SetScene.cpp にゲームループを追加

--- PacManAblityPanic/SetScene.cpp
+++ PacManAblityPanic/SetScene.cpp
@@ -1,1 +1,49 @@
-﻿
+﻿#include<stdio.h>
+#include"Main.h"
+#include"Scene.h"
+
+extern int g_SceneStep;
+
+void InitTitleScene();
+void UpdateTitleScene();
+int FinisTitleScene();
+
+int SetSceneMain()
+{
+	switch (g_SceneStep)
+	{
+		// 初期化
+	case SceneStep::InitStep:
+		InitTitleScene();
+		break;
+		// 本編
+	case SceneStep::MainStep:
+		UpdateTitleScene();
+		break;
+		// 終了
+	case SceneStep::EndStep:
+		return FinisTitleScene();
+		break;
+	}
+	return SET_SCENE_ID;
+}
+
+// 描画設定等
+void InitSetScene()
+{
+	g_SceneStep = SceneStep::MainStep;
+}
+
+// 次のシーンに行くための条件記入
+void UpdateSetScene()
+{
+
+	g_SceneStep = SceneStep::EndStep;
+}
+
+// 次に飛ぶシーン先の設定
+int FinisSetScene()
+{
+	// 次のシーンの遷移先IDを返す
+	return OPTION_SCENE_ID;
+}
