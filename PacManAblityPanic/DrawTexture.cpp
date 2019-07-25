045a792decc00cf23a9dc93c80cd83f17ef0c6cb
mono1140 <11potato30@gmail.com>
2019-07-26 00:13:42 +0900

    :fire: :boom: :sparkles:
    すでに作成されていた LoadTexture の引数の修正
    
    DrawTexture を修正し、最低限の引数で呼べる DrawTexture 関数と、細部まで設定できる DrawTextureEx 関数の作成
    
    その他、GetTexture 関数、 Check 関数などの追加、細分化

--- PacManAblityPanic/DrawTexture.cpp
+++ PacManAblityPanic/DrawTexture.cpp
@@ -4,28 +4,74 @@
 #include "DrawTexture.h"
 #include <D3dx9tex.h>
 
-TEXTUREDATA g_TextureList[MaxTextureId] = { 0 };
+static TEXTUREDATA** g_TextureList[SceneIdMax] = { nullptr };
+
+bool IsCategoryRangeCheck(int SceneId, int 	SelectTexture);
+bool CreateTexture(const char* file_name, int TextureWidth, int TextureHeight, TEXTUREDATA* texture_data);
+
+int SceneMaxTexture[] =
+{
+	TitleTextureList::MaxTitleTexture,
+	SetTextureList::MaxSetTexture,
+	OptionTextureList::MaxOptionTexture,
+	GameTextureList::MaxGameTexture,
+	ResultTextureList::MaxResultTexture,
+};
 
 Pointa D;
 
-bool LoadTexture(char* file_name, MAPCHIP_TEXTURE_ID mapchipnumber)
+bool LoadTexture(const char*    file_name, // ファイルの名前
+	                    int       SceneId, // SceneId
+	                    int SelectTexture, // MAPCHIP_TEXTURE_ID 
+	                    int  TextureWidth, // textureの幅
+	                    int  TextureHeight // textureの高さ
+)
 {
-	//IDに問題があればFALSEをかえす。
-	if (mapchipnumber <= MAPCHIP_TEXTURE_ID::Non &&
-		mapchipnumber >= MAPCHIP_TEXTURE_ID::MaxTextureId)
-	{
-		return false;
-	}
+	IsCategoryRangeCheck(SceneId,SelectTexture);
 
-	if (g_TextureList[mapchipnumber].m_pTexture != nullptr)
-	{
-		return true;
-	}
+	return CreateTexture(file_name,TextureWidth,TextureHeight, g_TextureList[SceneId][SelectTexture]);
+}
 
+void DrawTexture(float x, float y,float width, float height,TEXTUREDATA* texture)
+{
+	CUSTOMVERTEX cv[] = 
+	{
+		{x         ,y         ,  0.0f,  1.0f,    0xffffffff,  0.0f,  0.0f},
+		{x + width ,y         ,  0.0f,  1.0f,    0xffffffff,  1.0f,  0.0f},
+		{x + width ,y + height,  0.0f,  1.0f,    0xffffffff,  1.0f,  1.0f},
+		{x         ,y + height,  0.0f,  1.0f,    0xffffffff,  0.0f,  1.0f}
+	};								      			    
+
+	D.pDevice->SetFVF(D3DFVF_XYZRHW | D3DFVF_TEX1);
+	D.pDevice->SetTexture(0, texture->m_pTexture);
+	D.pDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, cv, sizeof(CUSTOMVERTEX));
+
+}
+
+void DrawTextureEx(float x, float y, float tu, float tv, float width, float height,TEXTUREDATA* texture)
+{
+	CUSTOMVERTEX cv[] =
+	{
+		{x         ,y         ,  0.0f,  1.0f,    0xffffffff,  tu,   tv},
+		{x + width ,y         ,  0.0f,  1.0f,    0xffffffff,  tu,   tv},
+		{x + width ,y + height,  0.0f,  1.0f,    0xffffffff,  tu,   tv},
+		{x         ,y + height,  0.0f,  1.0f,    0xffffffff,  tu,   tv}
+	};
+
+	D.pDevice->SetFVF(D3DFVF_XYZRHW | D3DFVF_TEX1);
+	D.pDevice->SetTexture(0, texture->m_pTexture);
+	D.pDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, cv, sizeof(CUSTOMVERTEX));
+
+}
+
+bool CreateTexture(const char* file_name,int TextureWidth, int TextureHeight, TEXTUREDATA* texture_data)
+{
 	// テクスチャ読み込み
-	if (FAILED(D3DXCreateTextureFromFileEx(D.pDevice, file_name, 450, 351, 0, 0, D3DFMT_UNKNOWN,
-		D3DPOOL_DEFAULT, D3DX_FILTER_NONE, D3DX_DEFAULT,
-		0xff000000, NULL, NULL, &g_TextureList[mapchipnumber].m_pTexture)))
+	if (FAILED(D3DXCreateTextureFromFileEx(D.pDevice, file_name,
+		TextureWidth, TextureHeight, 0, 0,
+		D3DFMT_UNKNOWN, D3DPOOL_DEFAULT, D3DX_FILTER_NONE, D3DX_DEFAULT, 0xff000000,
+		NULL, NULL, &texture_data->m_pTexture
+	)))
 	{
 		MessageBox(0, _T("テクスチャオブジェクトの作成に失敗しました"), NULL, MB_OK);
 		//return E_FAIL;
@@ -37,32 +83,47 @@ bool LoadTexture(char* file_name, MAPCHIP_TEXTURE_ID mapchipnumber)
 
 
 		// テクスチャ情報の取得
-		if (FAILED(g_TextureList[mapchipnumber].m_pTexture->GetLevelDesc(0, &desc)))
+		if (FAILED(texture_data->m_pTexture->GetLevelDesc(0, &desc)))
 		{
 			// 取得に失敗したら解放して読み込みを失敗とする
-			g_TextureList[mapchipnumber].m_pTexture->Release();
-			g_TextureList[mapchipnumber].m_pTexture = nullptr;
+			texture_data->m_pTexture->Release();
+			texture_data->m_pTexture = nullptr;
 			return false;
 		}
 		// サイズを保存
-		g_TextureList[mapchipnumber].m_Width = (float)desc.Width;
-		g_TextureList[mapchipnumber].m_Height = (float)desc.Height;
+		texture_data->m_Width = TextureWidth;
+		texture_data->m_Height = TextureHeight;
+
+		return true;
+	}
+}
+
+bool IsCategoryRangeCheck(int SceneId, int 	SelectTexture)
+{
+	
+	//IDに問題があればFALSEをかえす。
+	if (SceneId <= SceneId::TitleScene &&
+		SceneId >= SceneId::SceneIdMax)
+	{
+		return false;
+	}
+
+	if (SelectTexture <= -1 ||
+		SelectTexture >= SceneMaxTexture[SelectTexture])
+
+	{
+		return false;
 	}
 
 	return true;
 }
 
-void DrawTexture(FLOAT x, FLOAT y, FLOAT z ,FLOAT rhw,DWORD color , FLOAT tu , FLOAT tv,
-	FLOAT width, FLOAT height, FLOAT w_tu , FLOAT h_tv, INT texture)
+TEXTUREDATA* GetTexture(int SceneId, int SelectTexture)
 {
-	CUSTOMVERTEX cv[] = {
-		{x,y,z,rhw,color,tu,tv},
-		{x + width ,y,z,rhw,color,tu,tv},
-		{x + width,y + height,z,rhw,color,tu,tv},
-		{x,y + height,z,rhw,color,tu,tv}
-	};
+	if (IsCategoryRangeCheck(SceneId, SelectTexture) == false)
+	{
+		return nullptr;
+	}
 
-	D.pDevice->SetTexture(0, g_TextureList[texture].m_pTexture);
-	D.pDevice->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, cv, sizeof(CUSTOMVERTEX));
-
+	return g_TextureList[SceneId][SelectTexture];
 }
