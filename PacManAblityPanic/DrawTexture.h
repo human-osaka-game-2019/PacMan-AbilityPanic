cb2d549a69415c6615c026dcf860db0f697bf87a
mono1140 <11potato30@gmail.com>
2019-07-26 00:08:04 +0900

    :sparkles: DrowTexture.h に新しく作成した関数のプロトタイプ宣言を追加

--- PacManAblityPanic/DrawTexture.h
+++ PacManAblityPanic/DrawTexture.h
@@ -1,11 +1,13 @@
 ﻿#ifndef DRAWTEXTURE
 #define DRAWTEXTURE
-bool LoadTexture(char* file_name, MAPCHIP_TEXTURE_ID mapchipnumber);
-void DrawTexture(FLOAT x, FLOAT y, FLOAT z, FLOAT rhw, DWORD color, FLOAT tu, FLOAT tv,
-	FLOAT width, FLOAT height, FLOAT w_tu, FLOAT h_tv, INT texture);
 
+bool LoadTexture(const char* file_name, int SceneId , int mapchipnumber,int TextureWidth ,int TextureHeight);
 
+void DrawTexture(float x, float y,float width, float height,TEXTUREDATA* texture);
 
+void DrawTextureEx(float x, float y, float tu, float tv, float width, float height, TEXTUREDATA* texture);
+
+TEXTUREDATA* GetTexture(int SceneId, int SelectTexture);
 
 #endif // !1
 
