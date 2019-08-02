24444f9e3792d5c63292b511d0004f66d98da365
mono1140 <11potato30@gmail.com>
2019-08-02 22:47:38 +0900

    :sparkles: ゲームシステムに関する関数を入れる GameSystem.h GameSystem.cpp を作成

---
+++ PacManAblityPanic/GameSystem.cpp
@@ -0,0 +1,53 @@
+﻿#include"GameSystem.h"
+
+void CharTextureMove(Count* count, VariableNumber* var,int MapChipList[22][19])
+{
+	if (count->Frame < 20)
+	{
+		switch (var->Keystate)
+		{
+		case 0:
+			count->Frame = 0;
+
+			break;
+		case 1:
+
+			MapChipList[16][9] = 38;
+
+			if (10 < count->Frame && count->Frame < 20)
+			{
+				MapChipList[16][9] = 36;
+			}
+			break;
+
+		case 2:
+
+			MapChipList[16][9] = 39;
+
+			if (10 < count->Frame && count->Frame < 20)
+			{
+				MapChipList[16][9] = 36;
+			}
+			break;
+		case 3:
+
+			MapChipList[16][9] = 40;
+
+			if (10 < count->Frame && count->Frame < 20)
+			{
+				MapChipList[16][9] = 36;
+			}
+			break;
+		case 4:
+
+			MapChipList[16][9] = 37;
+
+			if (10 < count->Frame && count->Frame < 20)
+			{
+				MapChipList[16][9] = 36;
+			}
+			break;
+		}
+	}
+
+}
---
+++ PacManAblityPanic/GameSystem.h
@@ -0,0 +1,8 @@
+﻿#ifndef GAMESYSTEM_H
+#define GAMESYSTEM_H
+
+#include"class.h"
+
+void CharTextureMove(Count* count, VariableNumber* var, int MapChipList[22][19]);
+
+#endif // !1
