f8a204284e750efe7acd0a3d9f4e711ee43739d8
mono1140 <11potato30@gmail.com>
2019-08-04 04:20:47 +0900

    :sparkles: パックマンクラスのメンバ、関数の追加

--- PacManAblityPanic/GameScene.h
+++ PacManAblityPanic/GameScene.h
@@ -19,12 +19,36 @@ class PLAYER
 {
 public:
 
-	float pos_X;
-	float pos_Y;
+	float pos_X; // 描画のX座標
+	float pos_Y; // 描画のY座標
+
+	float pos_Tu; // 統合画像から切り取った時のX座標
+	float pos_Tv; // 統合画像から切り取った時のY座標
+
+	float TextureSize; // テクスチャの大きさ
+	float TuTvSize  = 0.078125;    // 統合画像から切り取った時の大きさ
+
+	void CoordinatesInput(int x, int y)
+	{
+		pos_X = x;
+		pos_Y = y;
+	};
+
+	void Initialize()
+	{
+	
+		pos_X = 958;
+		pos_Y = 760;
+		pos_Tu = 0.03125;
+		pos_Tv = 0.265625;
+
+		TextureSize = 40;
+		TuTvSize = 0.078125;
+	}
 
 };
 
-class ENEMY : public PLAYER 
+class ENEMY : public PLAYER
 {
 public:
 
