e5ade4fad8027720f081267e27a79a63b57189c4
mono1140 <11potato30@gmail.com>
2019-08-04 04:18:56 +0900

    :sparkles: 描画種類の変更による関数の変更、パックマンの操作の追加

--- PacManAblityPanic/GameSystem.cpp
+++ PacManAblityPanic/GameSystem.cpp
@@ -1,6 +1,7 @@
 ﻿#include"GameSystem.h"
+#include"GameScene.h"
 
-void CharTextureMove(Count* count, VariableNumber* var,int MapChipList[22][19])
+void CharTextureMove(Count* count, VariableNumber* var,int MapChipList[22][19], PLAYER* Pac_man)
 {
 	if (count->Frame < 20)
 	{
@@ -12,42 +13,69 @@ void CharTextureMove(Count* count, VariableNumber* var,int MapChipList[22][19])
 			break;
 		case 1:
 
-			MapChipList[16][9] = 38;
+			Pac_man->pos_Tu = (float)0.03125 + Pac_man->TuTvSize * 2;
 
 			if (10 < count->Frame && count->Frame < 20)
 			{
-				MapChipList[16][9] = 36;
+				Pac_man->pos_Tu = 0.03125;
 			}
 			break;
 
 		case 2:
 
-			MapChipList[16][9] = 39;
+			
+
+			Pac_man->pos_Tu = (float)0.03125 + Pac_man->TuTvSize * 3;
 
 			if (10 < count->Frame && count->Frame < 20)
 			{
-				MapChipList[16][9] = 36;
+				Pac_man->pos_Tu = 0.03125;
 			}
 			break;
 		case 3:
 
-			MapChipList[16][9] = 40;
+			
+
+			Pac_man->pos_Tu = (float)0.03125 + Pac_man->TuTvSize * 4;
 
 			if (10 < count->Frame && count->Frame < 20)
 			{
-				MapChipList[16][9] = 36;
+				Pac_man->pos_Tu = 0.03125;
 			}
 			break;
 		case 4:
 
-			MapChipList[16][9] = 37;
+			
+
+			Pac_man->pos_Tu = (float)0.03125 + Pac_man->TuTvSize * 1;
 
 			if (10 < count->Frame && count->Frame < 20)
 			{
-				MapChipList[16][9] = 36;
+				Pac_man->pos_Tu = 0.03125;
 			}
 			break;
 		}
 	}
 
+}
+
+void Pac_Mon_Move(VariableNumber* var, PLAYER* Pac_man)
+{
+	switch (var->Keystate)
+	{
+	case 0:
+		break;
+	case 1:
+		Pac_man->pos_Y = Pac_man->pos_Y - var->MoveSpeed;
+		break;
+	case 2:
+		Pac_man->pos_Y = Pac_man->pos_Y + var->MoveSpeed;
+		break;
+	case 3:
+		Pac_man->pos_X = Pac_man->pos_X + var->MoveSpeed;
+		break;
+	case 4:
+		Pac_man->pos_X = Pac_man->pos_X - var->MoveSpeed;
+		break;
+	}
 }
--- PacManAblityPanic/GameSystem.h
+++ PacManAblityPanic/GameSystem.h
@@ -2,7 +2,9 @@
 #define GAMESYSTEM_H
 
 #include"class.h"
+#include"GameScene.h"
 
-void CharTextureMove(Count* count, VariableNumber* var, int MapChipList[22][19]);
+void CharTextureMove(Count* count, VariableNumber* var, int MapChipList[22][19], PLAYER* Pac_man);
 
+void Pac_Mon_Move(VariableNumber* var, PLAYER* Pac_man);
 #endif // !1
