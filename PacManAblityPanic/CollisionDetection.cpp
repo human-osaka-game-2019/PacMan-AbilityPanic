592cff779c80884b6e69ebb3ef82e68499ca54d5
上久保豪太 <kamikubo1123@gmail.com>
2019-08-05 14:13:05 +0900

    :sparkles: 当たり判定用関数を追加

---
+++ PacManAblityPanic/CollisionDetection.cpp
@@ -0,0 +1,41 @@
+﻿#include"class.h"
+#include"CollisionDetection.h"
+
+NowStateId CollisionDetectionMapChip (MapChipData mapchip,float now_x, float now_y, int push_key,int MapChipList[MAP_SIZE_HEIGHT][MAP_SIZE_WIDTH])
+{
+
+	if ((int)now_x % (int)mapchip.MapChipWidht == 0 && (int)now_y % (int)mapchip.MapChipHeight == 0) 
+	{
+		int x = now_x / mapchip.MapChipWidht;
+		int y = now_y / mapchip.MapChipHeight;
+
+		switch (push_key)
+		{
+		case Key::Right:
+			if (MapChipList[y][x + 1] != 0 && MapChipList[y][x + 1] != 12) 
+			{
+				return Appulse;
+			}
+			else return Null;
+		case Key::Left:
+			if (MapChipList[y][x - 1] != 0 && MapChipList[y][x - 1] != 12) 
+			{
+				return Appulse;
+			}
+			else return Null;
+		case Key::Down:
+			if (MapChipList[y + 1][x] != 0 && MapChipList[y + 1][x] != 12) 
+			{
+				return Appulse;
+			}
+			else return Null;
+		case Key::Up:
+			if (MapChipList[y - 1][x] != 0 && MapChipList[y - 1][x] != 12) 
+			{
+				return Appulse;
+			}
+			else return Null;
+		}
+	}
+	else return NowMovw;
+}
---
+++ PacManAblityPanic/CollisionDetection.h
@@ -0,0 +1,38 @@
+﻿#ifndef COLLISON_DETECTION_H_
+#define COLLISON_DETECTION_H_
+
+const int MAP_SIZE_WIDTH = 19;
+const int MAP_SIZE_HEIGHT = 22;
+
+enum Key 
+{
+	Right,
+	Left,
+	Down,
+	Up
+};
+/**
+* @enum NowStateId
+* 当たり判定でのキャラクターの状態(クッキーは無視する)
+*/
+enum NowStateId
+{
+	//! キャラクターと衝突した
+	Appulse,
+	//! 現在移動中
+	NowMovw,
+	//! 目の前に何もない
+	Null
+};
+/**
+* @brief マップチップ用の当たり判定関数
+* @param (apChipData mapchip) マップチップサイズを含むクラス
+* @param (float now_x) キャラクターの現在のX座標
+* @param (float now_z) キャラクターの現在のY座標
+* @param (int push_key) キャラクターがどの方向を見ているか（進もうとしているか）
+* @param (int MapChipList[MAP_SIZE_HEIGHT][MAP_SIZE_WIDTH]) マップチップの二次元配列
+*/
+NowStateId CollisionDetectionMapChip(MapChipData mapchip, float now_x, float now_y, int push_key, int MapChipList[MAP_SIZE_HEIGHT][MAP_SIZE_WIDTH]);
+
+#endif // !COLLISON_DETECTION_H_
+
